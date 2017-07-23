328790334ded7809de2830cfc5cf8f726ee798b1
Eric Bäckhage <eric.backhage@gmail.com>
2017-07-23 23:24:12 +0200

    First commit

---
+++ HashTool/HashModule.fs
@@ -0,0 +1,26 @@
+﻿module HashModule
+
+open System.IO
+open System.Security.Cryptography
+
+type Hash (directory : string) =
+    let sha256 = SHA256Managed.Create()
+    let dirInfo = DirectoryInfo(directory)
+    let fileInfos = dirInfo.GetFiles()
+
+    let formatHashAsReadable hashValue =
+        hashValue |> Array.fold (fun acc data -> acc + (sprintf "%02X" data)) ""
+
+    let calcHash (fileInfo : FileInfo) =
+        try
+            use fileStream = fileInfo.Open(FileMode.Open)
+            fileStream.Position <- 0L
+            let hashValue = sha256.ComputeHash(fileStream)
+            
+            fileInfo.Name, formatHashAsReadable hashValue
+        with
+            | :? System.IO.IOException as ex -> fileInfo.Name, ex.Message
+            | :? System.UnauthorizedAccessException as ex -> fileInfo.Name, ex.Message
+
+    member this.PrintFileHashes () =
+        Array.toSeq fileInfos |> Seq.map (fun file -> calcHash file) |> Seq.iter (fun (fileName, hash) -> printfn "%s: %s" fileName hash) 
---
+++ HashTool/Program.fs
@@ -0,0 +1,18 @@
+﻿open System.IO
+
+[<EntryPoint>]
+let main argv = 
+    if Array.length argv <> 1 then
+        printfn "Usage: HashTool.exe dir"
+        exit 1
+    
+    let directory = argv.[0]
+
+    if not <| Directory.Exists(directory) then
+        printfn "ERROR: Directory '%s' does not exist" directory
+        exit 1
+
+    let hashes = HashModule.Hash directory
+    hashes.PrintFileHashes ()
+
+    0
